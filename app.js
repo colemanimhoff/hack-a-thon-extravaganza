0e4a67f5050ceb1ac7e94076f02376ba69feade4
Coleman Imhoff <colemanimhoff@gmail.com>
2018-04-27 16:00:07 -0600

    app is broken :(

--- app.js
+++ app.js
@@ -1,139 +1,116 @@
-const movieUrl = 'http://www.omdbapi.com/'
+const movieUrl = 'http://www.omdbapi.com/?t=Boogie Nights&apikey=39ddfcee'
 const emojis = document.querySelectorAll('.dropdown-item')
 const text = document.querySelector('#text')
 const chillButton = document.querySelector('#chill')
 let searchParam
 
 let sadMovies = [
-    'Schindler’s List', 
-    'Atonement', 
-    'The Land Before Time ', 
-    'The House Of Small Cubes', 
-    'Milk', 
-    'Welcome to the Dollhouse', 
-    'The Road', 
-    'Fruitvale Station', 
-    'Dead Poets Society', 
+    'Schindler’s List',
+    'Atonement',
+    'The Land Before Time ',
+    'The House Of Small Cubes',
+    'Milk',
+    'Welcome to the Dollhouse',
+    'The Road',
+    'Fruitvale Station',
+    'Dead Poets Society',
     'Extremis'
 ]
 
 let dramaMovies = [
-    'The Loft', 
-    'J. Edgar ', 
-    'The Negotiator', 
-    'Michael Clayton', 
-    'The Interview', 
-    'Gangs of New York', 
-    'Casino', 
-    'Goodfellas', 
-    'The Godfather ', 
+    'The Loft',
+    'J. Edgar ',
+    'The Negotiator',
+    'Michael Clayton',
+    'The Interview',
+    'Gangs of New York',
+    'Casino',
+    'Goodfellas',
+    'The Godfather ',
     'The Revenant'
 ]
 
 let romanticMovies = [
-    'Adventureland', 
-    'The Social Network', 
-    'Eternal Sunshine of the Spotless Mind', 
-    'Clueless', 
-    'The Graduate', 
-    'Drive', 
-    'Lost in Translation', 
-    'Brokeback Mountain', 
-    'Jerry Maguire', 
+    'Adventureland',
+    'The Social Network',
+    'Eternal Sunshine of the Spotless Mind',
+    'Clueless',
+    'The Graduate',
+    'Drive',
+    'Lost in Translation',
+    'Brokeback Mountain',
+    'Jerry Maguire',
     'The Wedding Singer'
 ]
 
 let actionMovies = [
-    'The Avengers', 
-    'Mission Impossible 2', 
-    'Mission Impossible: Ghost Protocol', 
-    'Desperado', 
-    'Braveheart', 
-    'Resevoir Dogs', 
-    'Butch Cassidy and the Sundance Kid', 
-    'Terminator 2: Judgement Day', 
-    'Ip Man', 
-    'Black Hawk Down', 
+    'The Avengers',
+    'Mission Impossible 2',
+    'Mission Impossible: Ghost Protocol',
+    'Desperado',
+    'Braveheart',
+    'Resevoir Dogs',
+    'Butch Cassidy and the Sundance Kid',
+    'Terminator 2: Judgement Day',
+    'Ip Man',
+    'Black Hawk Down',
     'Gladiator'
 ]
 
 let chickFlickMovies = [
-    'Legally Blonde', 
-    'Grease', 
-    'Love and Honor', 
-    'Mean Girls', 
-    'The Breakfast Club', 
-    '10 Things I Hate About You', 
-    'Moonrise Kingdom', 
-    'Charlie St. Cloud', 
-    'Can\'t Buy Me Love', 
-    'She\'s All That']
+    'Legally Blonde',
+    'Grease',
+    'Love and Honor',
+    'Mean Girls',
+    'The Breakfast Club',
+    '10 Things I Hate About You',
+    'Moonrise Kingdom',
+    'Charlie St. Cloud',
+    'Can\'t Buy Me Love',
+    'She\'s All That'
+]
 
 let comedyMovies = [
-    'Tommy Boy', 
-    'Planes, Trains and Automobiles', 
-    'The Trip', 'The Breakfast Club', 
+    'Tommy Boy',
+    'Planes, Trains and Automobiles',
+    'The Trip', 'The Breakfast Club',
     'Hot Rod',
-    'Happy Gilmore', 
-    'Zoolander', 
-    'Clerks', 
-    'Amelie']
+    'Happy Gilmore',
+    'Zoolander',
+    'Clerks',
+    'Amelie'
+]
 
 let scaryMovies = [
-    'The Babadook', 
-    'Hellraiser', 
-    'The Nightmare', 
-    'It Follows', 
-    'The Descent', 
-    'The Wailing', 
-    'Gerald’s Game', 
-    'The Sixth Sense', 
-    'Cube', 
+    'The Babadook',
+    'Hellraiser',
+    'The Nightmare',
+    'It Follows',
+    'The Descent',
+    'The Wailing',
+    'Gerald’s Game',
+    'The Sixth Sense',
+    'Cube',
     'Veronica'
 ]
 
-chillButton.addEventListener('click', function(event) {
-    parseEmoji(event)
-    buildMovieUrl(searchParam)
+chillButton.addEventListener('click', function (event) {
+    return fetch(movieUrl)
+        .then(response => response.json())
+        .then(movie => buildMovieCard(movie))
 })
 
-function randomIndex(number) {
-    return Math.floor(Math.random() * number)
+function buildMovieUrl(event, searchParam) {
+    const key = '39ddfcee'
+    let currentMovie = sadMovies[randomIndex(searchParam.length)].replace(/[' ']/g, '+')
+    console.log(currentMovie)
+    return movieUrl.concat(`?t=${currentMovie}&apikey=${key}`)
 }
 
-function buildMovieUrl(searchParam) {
-    console.log(searchParam)
-    // const key = '39ddfcee'
-    // let currentMovie = searchParam[randomIndex(searchParam.length)].replace(/[' ']/g, '+')
-    // console.log(currentMovie)
-    // return movieUrl.concat(`?t=${currentMovie}&apikey=${key}`)
-}
-
-function getData(url) {
+function getData(event, url) {
     return fetch(url)
-    .then(response => response.json())
-}
-
-function parseEmoji() {
-    emojis.forEach(emoji => {
-        emoji.addEventListener('click', function (event) { 
-            if (event.target.id === 'sad') {
-               searchParam = sadMovies
-            } else if (event.target.id === 'detective') {
-            searchParam = dramaMovies
-            } else if (event.target.id === 'laughing') {
-                searchParam = comedyMovies
-            } else if (event.target.id === 'bomb') {
-                searchParam = actionMovies
-            } else if (event.target.id === 'love') {
-                searchParam = romanticMovies
-            } else if (event.target.id === 'ghost') {
-                searchParam = scaryMovies
-            } else {
-                alert('Get a roooooooooom!')
-            }
-        })
-    })
+        .then(response => response.json())
+        .then(response => console.log(response))
 }
 
 function buildMovieCard(movie) {
@@ -180,11 +157,11 @@ function prependElement(parent, element) {
     return parent.prepend(element)
 }
 
-function addAttr (element, attr, value) {
+function addAttr(element, attr, value) {
     return element.setAttribute(attr, value)
 }
 
-function addText (element, string) {
+function addText(element, string) {
     return element.textContent = string
 }
 
@@ -194,4 +171,8 @@ function clearContainers(element) {
 
 function addClass(element, string) {
     return element.classList.add(string)
+}
+
+function randomIndex(number) {
+    return Math.floor(Math.random() * number)
 }
