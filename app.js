3a9475158bae8ee3d88dbe34bfb46758433d91e3
Coleman Imhoff <colemanimhoff@gmail.com>
2018-04-27 11:03:08 -0600

    added DOM functions to app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-console.log('test')
-const movieUrl = 'http://www.omdbapi.com/?t=Jaws&apikey=39ddfcee&'
+const movieUrl = 'http://www.omdbapi.com/?t=Boogie Nights&apikey=39ddfcee'
 
 getData(movieUrl)
 
@@ -7,4 +6,32 @@ function getData(url) {
     return fetch(url)
     .then(response => response.json())
     .then(response => console.log(response))
+}
+
+function buildMovieCard() {
+
+}
+
+function createElement(element) {
+    return document.createElement(element)
+}
+
+function appendElement(parent, element) {
+    return parent.appendChild(element)
+}
+
+function prependElement(parent, element) {
+    return parent.prepend(element)
+}
+
+function addAttr(element, attr, string) {
+    return element.setAttribute(attr, string)
+}
+
+function clearContainers(element) {
+    return element.innerHTML = ''
+}
+
+function addClass(element, string) {
+    return element.classList.add(string)
 }
